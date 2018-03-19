e096768158c9d0af05f46a43a1aeae80765ed02b
Rubatsiro Chiripa <rubatsiro.chiripa@econet.co.zw>
2018-03-19 14:51:34 +0200

    adding circleci config

---
+++ src/main/scripts/configure-mysql.sql
@@ -0,0 +1,28 @@
+# connect to mysql and run as root user
+# create DATABASES
+CREATE DATABASE sfg_dev;
+CREATE DATABASE sfg_prod;
+
+# create database service accounts
+CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'guru';
+CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'guru';
+CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'guru';
+CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'guru';
+
+# database grants i.e. only limited to DML OPs, no DDL
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'%';
